4b108800309a015032ee001833f1fb65a6645ddb
sa3520 <shaokunan1@gmail.com>
2024-03-24 22:05:44 -0400

    add core functions

--- cpp_src/MultipleFOVKDTree.h
+++ cpp_src/MultipleFOVKDTree.h
@@ -4,10 +4,24 @@
 class SpatialDataMultiple
 {
 public:
-    SpatialDataMultiple() {};
+    SpatialDataMultiple(){};
     // Build multiple FOV by adding FOV data to current FOV data
-    void addFOVData(int fovID, const py::array_t<float> &coordinates, const std::vector<std::string> &labels);
+    void setFOVData(int fovID, const py::array_t<float> &coordinates, const py::list &labels);
+    py::tuple buildFPTreeKNN(const Item &cellType, const py::list &fovIDs, int k = 30, float minSupport = 0.5, bool disDuplicates = false);
+    py::tuple buildFPTreeDist(const Item &cellType, const py::list &fovIDs, float radius = 100.0, float minSupport = 0.5, bool disDuplicates = false, int minSize = 0);
+    py::list motifEnrichmentKNN(const Item &cellType, const py::list &motifs, py::list &fovIDs, int k = 30, float minSupport = 0.5, bool disDuplicates = false, float maxDist = 200.0);
+    py::list motifEnrichmentDist(const Item &cellType, const py::list &motifs, py::list &fovIDs, float radius = 100.0, float minSupport = 0.5, bool disDuplicates = false, int minSize = 0);
+    py::list findFPKnnFOV(const std::string &cellType, const int fovID, int k = 30,
+                          int minCount = 0, float minSupport = 0.5);
+    std::pair<py::dict, py::dict>
+    differentialAnalysisKnn(const std::string &cellType,
+                            const py::list &datasets,
+                            const py::list &fovsID1,
+                            const py::list &fovsID2,
+                            int k = 30,
+                            int minCount = 0,
+                            float minSupport = 0.5);
 
-private:
+// private:
     std::unordered_map<int, SpatialDataSingle> fovs;
-}
+};
